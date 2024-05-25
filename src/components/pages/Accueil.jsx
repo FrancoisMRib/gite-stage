fe1e7d72dc7da77be7cac5b9c468759a5f72966e
FrancoisMRib <fr.ribere@gmail.com>
2024-05-26 01:40:09 +0200

    DÃAccueil fini

--- src/components/pages/Accueil.jsx
+++ src/components/pages/Accueil.jsx
@@ -1,28 +1,28 @@
 
 // import image1 from "gite-vite/src/img/IMG_20190803_162235.jpg";
 import './Accueil.css';
-import {Carousel} from './Carousel';
+import {Slider} from './Slider';
 
 export function Accueil() {
     return (
         <div>
-            <h2>Accueil</h2>
-            <div style={{
+            <h2 className="entree">Bienvenue sur le Gîte la Vigne !</h2>
+            <div className='accueil' style={{
             display:'flex',
             flex:'row'
         }}>
-            <Carousel/>
-            <div>
+            <Slider/>
+            {/* <div>
                 <img src="src/components/img/IMG_20190803_162256.jpg" alt="" className='carrousel'/>
                 <p>les vignes auxquelles le gîte doit son nom</p>
             </div>
             <div>
                 <img src="src/components/img/IMG_20190803_162400.jpg" alt="" className='carrousel'/>
                 <p>Les terrasses paysagères</p>
-            </div>
-            <div>
+            </div> */}
+            <div className='text'>
                 <div>
-                <p>Bienvenue au Gîte la Vigne !</p><br></br>
+                {/* <p>Bienvenue au Gîte la Vigne !</p><br></br> */}
                 <p>Nous sommes heureux de vous accueillir au sein de</p>
                 <p>notre établissement confortable et authentique. En</p>
                 <p>espérons que vous passerez un séjour des plus</p>
